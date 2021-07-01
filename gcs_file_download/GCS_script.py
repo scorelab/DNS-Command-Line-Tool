821b3aca745cb23d80ec7607fb187bbaee6c54d8
VinuriBandara <vinugayanthika@gmail.com>
2021-07-01 11:29:55 +0530

    Download complete dataset from GCS

--- gcs_file_download/GCS_script.py
+++ gcs_file_download/GCS_script.py
@@ -1,8 +1,6 @@
-import json
 import argparse 
 from google.cloud import storage
 import os
-import io
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google_app_cred.json"
 
@@ -23,13 +21,15 @@ def list_data(bucket_name):
         print(blob.name)
 
 
-def last_update(bucket_name, source_blob_name):
+def last_update(bucket_name):
 
     storage_client = storage.Client()
  
     bucket = storage_client.bucket(bucket_name)
-    blob = bucket.get_blob(source_blob_name)
-    print("Updated: {}".format(blob.updated))
+    # blob = bucket.get_blob(source_blob_name)
+    blobs = storage_client.list_blobs(bucket)
+    for blob in blobs:
+        print("{} updated on: {}".format(blob.name,blob.updated))
 
 def download_blob(bucket_name, source_blob_name, dest_file):
     storage_client = storage.Client()
@@ -44,6 +44,30 @@ def download_blob(bucket_name, source_blob_name, dest_file):
         )
     )
 
+
+def download_bucket(bucket_name,dest_folder):
+    if not os.path.exists(dest_folder):
+        os.mkdir(dest_folder)
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blobs = storage_client.list_blobs(bucket_name)
+
+    for blob in blobs:
+        with open(os.path.join(dest_folder, str(blob.name)), 'w') as fp:
+            pass
+        blob = bucket.blob(blob.name)
+        blob.download_to_filename(dest_folder+'/'+blob.name)
+ 
+        print(
+            "Blob {} downloaded to file path {} successfully ".format(
+                blob.name, dest_folder
+            )
+        )
+
+
+    
+
+
 bucket_object = 'dnstest_bucket_1'
 blob = 'blob_dns'
 
@@ -57,8 +81,9 @@ parser.add_argument('--list' ,  action='store_const',const='True',help="List the
 
 parser.add_argument('--last_update' , action='store_const',const='True', help="See when the dataset was last updated")
 
-parser.add_argument('--download' , help="First time download of the DNS IP dataset")
+parser.add_argument('--download' , help="First time download of the DNS IP blob")
 
+parser.add_argument('--download_bucket',help="First time download of the DNS IP dataset")
 
 args = vars(parser.parse_args())
 
@@ -72,9 +97,13 @@ if args.get('list'):
 
 
 if args.get('last_update'):
-    last_update(bucket_object,blob)
+    last_update(bucket_object)
 
 if args.get('download'):
     dest_file = args['download']
     download_blob(bucket_object,blob,dest_file)
+
+if args.get('download_bucket'):
+    dest_folder = args['download_bucket']
+    download_bucket(bucket_object,dest_folder)
 
